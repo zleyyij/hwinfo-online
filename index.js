bfbda3cf87a806fa78a5b841757e14fa1411a6c1
James Boehme <jboehme05@gmail.com>
2022-06-12 16:34:45 -0600

    seals wanted formatting

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//neat little object where all results are kept
+    //neat little object where all results are kept
     let formObj = {};
     //data that is not graphed, EG(Time)
     //actually currently not used, but might later
@@ -6,38 +6,31 @@
     //Global config options
     let drawGraphConf = {
 	xTime: true,
-    }
+    };
 
-                    //time, data, name of div to insert graph, config
+    //time, data, name of div to insert graph, config
     function drawGraph(srs=[], divName, conf) {
 	/* Current config options:
 	     * xTime: search for time within the parsed csv, and try to use it instead of datapoints on the x axis
-	     *
-	     *
-	     * */
+	*/
 	let graphSettings = {
-            boost: {
-                useGPUTranslations: true
-            },
-
+		boost: {
+			useGPUTranslations: true
+			},
 		title: {
 			text: divName
 		},
 		xAxis: {
 			tickInterval: 100
 		},	
-
 		series: srs
-
-
-	}	
-	    //see if there's an array called Time if xTime is true(should be for all hwinfo logs)
-	    //in the same line see if the Time element exists in formObj
-	    if(conf.xTime == true && "Time" in formObj){
-		    graphSettings.xAxis.categories = stripMs(formObj["Time"]);
-
-	    }
-	    Highcharts.chart(divName, graphSettings);
+	};	
+	//see if there's an array called Time if xTime is true(should be for all hwinfo logs)
+	//in the same line see if the Time element exists in formObj
+	if(conf.xTime == true && "Time" in formObj){
+		graphSettings.xAxis.categories = stripMs(formObj["Time"]);
+	}
+	Highcharts.chart(divName, graphSettings);
     }
 //stripping ms with regex cuz it looks nice 
 //this function is only used exactly *one* time 
@@ -63,32 +56,30 @@ function parseCSV() {
 			let f = results.data;
 			//f[0] is the title of each column
 			for (let i in f[0]) {
-                    formObj[f[0][i]] = [];
-                }
-                //looping through, demessing up the arrays
-                for (let i = 1; i < f.length; i++) {
-                    for (let j in f[0])
-                        formObj[f[0][j]].push(f[i][j]);
-                }
-                //this is either an artifact from the log, the parser, or my bad code
-                delete formObj[""];
-
-		
-
-		//going through, converting number-strings("1") to numbers(1)
-		for(let i in formObj){
-			//I think it's always the last 5 using this specific parser
-			//so will pop for now, should probably come up with a context match instead
-			//for the record I did try to use splice, but that was behaving very oddly
-		//	for(let j = 0; j < 10; j++){
-				formObj[i].pop();
-		//	}
-			for(let j = 0; j < formObj[i].length; j++){
-				if(!Number.isNaN(Number(formObj[i][j]))){
-					formObj[i][j] = Number(formObj[i][j]);
+				formObj[f[0][i]] = [];
+			}
+			//looping through, demessing up the arrays
+			//o(n^2) efficiency right here bb
+			for (let i = 1; i < f.length; i++) {
+				for (let j in f[0]){
+					formObj[f[0][j]].push(f[i][j]);
 				}
-				
-			}	
+			}
+			//this is either an artifact from the log, the parser, or my bad code
+			delete formObj[""];
+
+
+			//going through, converting number-strings("1") to numbers(1)
+			for(let i in formObj){
+			//for the record I did try to use splice, but that was behaving very oddly
+				//for(let j = 0; j < 10; j++){
+				formObj[i].pop();
+				//}
+				for(let j = 0; j < formObj[i].length; j++){
+					if(!Number.isNaN(Number(formObj[i][j]))){
+						formObj[i][j] = Number(formObj[i][j]);
+					}
+				}	
 	
 		}
 		//moving time to the nongraphed array
